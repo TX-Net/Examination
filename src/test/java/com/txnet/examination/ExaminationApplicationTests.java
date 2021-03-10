0f266fd0f0ceea082bed6a6c311e0cc767cbab66
wangkun <aissue@126.com>
2021-03-10 13:43:10 +0800

    init commit

---
+++ src/test/java/com/txnet/examination/ExaminationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.txnet.examination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
